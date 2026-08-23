7f7121a21213efd942e971d78c9e065d22efbf3b
Ishaan1604 <163505826+Ishaan1604@users.noreply.github.com>
2026-08-23 17:47:03 +0100

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict1 = defaultdict(int)
+        dict2 = defaultdict(int)
+        for c in s:
+            dict1[c] += 1
+        for d in t:
+            dict2[d] += 1
+        
+        return dict1 == dict2
+        
